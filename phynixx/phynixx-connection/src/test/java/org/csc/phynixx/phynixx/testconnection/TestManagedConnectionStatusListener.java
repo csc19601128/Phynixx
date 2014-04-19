7ba6dd539be7977a6a7d14dd2efa6a1abdb96b4f
christoph <phynixx@freenet.de>
2014-04-19 16:54:48 +0200

    Signed-off-by: christoph <phynixx@freenet.de>

---
+++ phynixx/phynixx-connection/src/test/java/org/csc/phynixx/phynixx/testconnection/TestManagedConnectionStatusListener.java
@@ -0,0 +1,87 @@
+/**
+ *
+ */
+package org.csc.phynixx.phynixx.testconnection;
+
+/*
+ * #%L
+ * phynixx-connection
+ * %%
+ * Copyright (C) 2014 csc
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import org.csc.phynixx.connection.*;
+
+public class TestManagedConnectionStatusListener extends PhynixxManagedConnectionListenerAdapter<ITestConnection> implements IPhynixxManagedConnectionDecorator<ITestConnection> {
+
+
+    @Override
+    public void connectionCommitted(IManagedConnectionCommitEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.COMMITTED);
+    }
+
+    @Override
+    public void connectionReset(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.RESET);
+    }
+
+    @Override
+    public void connectionReleased(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.RELEASED);
+    }
+
+    @Override
+    public void connectionFreed(IManagedConnectionEvent<ITestConnection> event) {
+        try {
+            TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.FREED);
+        } finally {
+            event.getManagedConnection().removeConnectionListener(this);
+        }
+    }
+
+    @Override
+    public void connectionErrorOccurred(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.ERROR_OCCURRED);
+    }
+
+    @Override
+    public void connectionRequiresTransaction(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.REQUIRES_TRANSACTION);
+    }
+
+    @Override
+    public void connectionPrepared(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.PREPARED);
+    }
+
+    @Override
+    public void connectionRolledback(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.ROLLEDBACK);
+    }
+
+    @Override
+    public void connectionRecovered(IManagedConnectionEvent<ITestConnection> event) {
+        TestConnectionStatusManager.registerStatus(event.getManagedConnection(), TestConnectionStatus.RECOVERED);
+    }
+
+    public IPhynixxManagedConnection decorate(IPhynixxManagedConnection<ITestConnection> managedConnection) {
+        managedConnection.addConnectionListener(this);
+        return managedConnection;
+
+    }
+
+}
